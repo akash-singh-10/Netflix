499f6cbbebe0d7051faf18aa6f0b0ea562744f9c
Akash Singh <akashsinghit2024@gmail.com>
2023-05-11 21:41:18 +0530

    login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -19,4 +19,26 @@ router.post("/register", async (req, res) => {
     }
 });
 
+// LOGIN
+router.post("/login", async (req, res) => {
+    try{
+        const user = await User.findOne({ email: req.body.email });
+        !user && res.status(401).json("Wrong Password or Username!");
+
+        const bytes  = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+        originalPassword != res.body.password &&
+            res.status(401).json("Wrong Password or Username!");
+
+        const {password, ...info} = user._doc;
+
+        res.status(200).json(info);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
 module.exports = router;
